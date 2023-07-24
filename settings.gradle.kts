10a5244c997ad3343bfab3fdd618071d32bd9d3a
Konstantin Kolchurin <kkolchurin@icerockdev.com>
2023-07-24 17:33:58 +0700

    MOKO-556 add Usage block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
